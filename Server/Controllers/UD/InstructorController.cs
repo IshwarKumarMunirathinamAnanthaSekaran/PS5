46222cdefc10d4dceeb4e489ad3c51dd710169b9
IshwarKumarMunirathinamAnanthaSekaran <maishwar@udel.edu>
2023-04-11 10:54:06 -0400

    Zipcode and Instructor changes

---
+++ Server/Controllers/UD/InstructorController.cs
@@ -0,0 +1,223 @@
+﻿using DOOR.EF.Data;
+using DOOR.EF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Net.Http.Headers;
+using System.Drawing;
+using Microsoft.AspNetCore.Identity;
+using DOOR.Server.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Data;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Numerics;
+using DOOR.Shared.DTO;
+using DOOR.Shared.Utils;
+using DOOR.Server.Controllers.Common;
+using Microsoft.Extensions.Hosting;
+using static Duende.IdentityServer.Models.IdentityResources;
+
+namespace DOOR.Server.Controllers.UD
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InstructorController: BaseController
+	{
+        public InstructorController(DOOROracleContext _DBcontext,
+    OraTransMsgs _OraTransMsgs)
+    : base(_DBcontext, _OraTransMsgs)
+
+        {
+        }
+
+        [HttpGet]
+        [Route("GetInstructor")]
+        public async Task<IActionResult> GetInstructor()
+        {
+            List<InstructorDTO> lst = await _context.Instructors
+                .Select(sp => new InstructorDTO
+                {
+                    SchoolId = sp.SchoolId,
+                    InstructorId = sp.InstructorId,
+                    Salutation = sp.Salutation,
+                    FirstName = sp.FirstName,
+                    LastName = sp.LastName,
+                    StreetAddress = sp.StreetAddress,
+                    Zip = sp.Zip,
+                    Phone = sp.Phone,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate
+                }).ToListAsync();
+            return Ok(lst);
+        }
+
+        [HttpGet]
+        [Route("GetInstructor/{_SchoolID}/{_InstructorID}")]
+        public async Task<IActionResult> GetInstructor(int _SchoolID,int _InstructorID)
+        {
+            InstructorDTO? lst = await _context.Instructors
+                .Where(x => x.SchoolId == _SchoolID && x.InstructorId == _InstructorID)
+                .Select(sp => new InstructorDTO
+                {
+                    SchoolId = sp.SchoolId,
+                    InstructorId = sp.InstructorId,
+                    Salutation = sp.Salutation,
+                    FirstName = sp.FirstName,
+                    LastName = sp.LastName,
+                    StreetAddress = sp.StreetAddress,
+                    Zip = sp.Zip,
+                    Phone = sp.Phone,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate
+                }).FirstOrDefaultAsync();
+            return Ok(lst);
+        }
+
+        [HttpPost]
+        [Route("PostInstructor")]
+        public async Task<IActionResult> PostEnrollment([FromBody] InstructorDTO _InstructorDTO)
+        {
+            try
+            {
+               Instructor c = await _context.Instructors.Where(x => x.SchoolId == _InstructorDTO.InstructorId && x.InstructorId == _InstructorDTO.InstructorId).FirstOrDefaultAsync();
+
+                if (c == null)
+                {
+                    c = new Instructor
+                    {
+
+
+                        
+                        SchoolId = _InstructorDTO.SchoolId,
+                        InstructorId = _InstructorDTO.InstructorId,
+                        Salutation = _InstructorDTO.Salutation,
+                        FirstName = _InstructorDTO.FirstName,
+                        LastName = _InstructorDTO.LastName,
+                        StreetAddress = _InstructorDTO.StreetAddress,
+                        Zip = _InstructorDTO.Zip,
+                        Phone = _InstructorDTO.Phone,
+                        CreatedBy = _InstructorDTO.CreatedBy,
+                        CreatedDate = _InstructorDTO.CreatedDate,
+                        ModifiedBy = _InstructorDTO.ModifiedBy,
+                        ModifiedDate = _InstructorDTO.ModifiedDate
+
+                    };
+                    _context.Instructors.Add(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("PutInstructor")]
+        public async Task<IActionResult> PutCourse([FromBody] InstructorDTO _InstructorDTO)
+        {
+            try
+            {
+                Instructor c = await _context.Instructors.Where(x => x.SchoolId == _InstructorDTO.SchoolId && x.InstructorId == _InstructorDTO.InstructorId).FirstOrDefaultAsync();
+
+                if (c != null)
+                {
+                    
+                    c.SchoolId = _InstructorDTO.SchoolId;
+                    c.InstructorId = _InstructorDTO.InstructorId;
+                    c.Salutation = _InstructorDTO.Salutation;
+                    c.FirstName = _InstructorDTO.FirstName;
+                    c.LastName = _InstructorDTO.LastName;
+                    c.StreetAddress = _InstructorDTO.StreetAddress;
+                    c.Zip = _InstructorDTO.Zip;
+                    c.Phone = _InstructorDTO.Phone;
+                    c.CreatedBy = _InstructorDTO.CreatedBy;
+                    c.CreatedDate = _InstructorDTO.CreatedDate;
+                    c.ModifiedBy = _InstructorDTO.ModifiedBy;
+                    c.ModifiedDate = _InstructorDTO.ModifiedDate;
+
+                    _context.Instructors.Update(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("DeleteInstructor/{_SchoolID}/{_InstructorID}")]
+        public async Task<IActionResult> DeleteInstructor(int _SchoolID, int _InstructorID)
+        {
+            try
+            {
+                Instructor c = await _context.Instructors.Where(x => x.SchoolId == _SchoolID && x.InstructorId == _InstructorID).FirstOrDefaultAsync();
+
+                if (c != null)
+                {
+                    _context.Instructors.Remove(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+    }
+}
+
---
+++ Server/Controllers/UD/ZipcodeController.cs
@@ -0,0 +1,207 @@
+﻿using DOOR.EF.Data;
+using DOOR.EF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Net.Http.Headers;
+using System.Drawing;
+using Microsoft.AspNetCore.Identity;
+using DOOR.Server.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Data;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System.Numerics;
+using DOOR.Shared.DTO;
+using DOOR.Shared.Utils;
+using DOOR.Server.Controllers.Common;
+using Microsoft.Extensions.Hosting;
+
+namespace DOOR.Server.Controllers.UD
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ZipcodeController: BaseController
+    {
+        public ZipcodeController(DOOROracleContext _DBcontext,
+    OraTransMsgs _OraTransMsgs)
+    : base(_DBcontext, _OraTransMsgs)
+
+        {
+        }
+
+        [HttpGet]
+        [Route("GetZipcode")]
+        public async Task<IActionResult> GetZipcode()
+        {
+            List<ZipcodeDTO> lst = await _context.Zipcodes
+                .Select(sp => new ZipcodeDTO
+                {
+                    Zip = sp.Zip,
+                    City = sp.City,
+                    State = sp.State,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate
+
+                }).ToListAsync();
+            return Ok(lst);
+        }
+
+        [HttpGet]
+        [Route("GetZipcode/{_Zip}")]
+        public async Task<IActionResult> GetZipcode(String _Zip)
+        {
+            ZipcodeDTO? lst = await _context.Zipcodes
+                .Where(x => x.Zip == _Zip)
+                .Select(sp => new ZipcodeDTO
+                {
+                    Zip = sp.Zip,
+                    City = sp.City,
+                    State = sp.State,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate
+
+                }).FirstOrDefaultAsync();
+            return Ok(lst);
+        }
+
+        [HttpPost]
+        [Route("PostZipcode")]
+        public async Task<IActionResult> PostZipcode([FromBody] ZipcodeDTO _ZipcodeDTO)
+        {
+            try
+            {
+                Zipcode c = await _context.Zipcodes.Where(x => x.Zip == _ZipcodeDTO.Zip).FirstOrDefaultAsync();
+
+                if (c == null)
+                {
+                    c = new Zipcode
+                    {
+
+
+                        Zip = _ZipcodeDTO.Zip,
+                        City = _ZipcodeDTO.City,
+                        State = _ZipcodeDTO.State,
+                        CreatedBy = _ZipcodeDTO.CreatedBy,
+                        CreatedDate = _ZipcodeDTO.CreatedDate,
+                        ModifiedBy = _ZipcodeDTO.ModifiedBy,
+                        ModifiedDate = _ZipcodeDTO.ModifiedDate
+                    };
+
+                    _context.Zipcodes.Add(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+
+
+        [HttpPut]
+        [Route("PutZipcode")]
+        public async Task<IActionResult> PutZipcode([FromBody] ZipcodeDTO _ZipcodeDTO)
+        {
+            try
+            {
+                Zipcode c = await _context.Zipcodes.Where(x => x.Zip == _ZipcodeDTO.Zip).FirstOrDefaultAsync();
+
+                if (c != null)
+                {
+                    
+                    c.Zip = _ZipcodeDTO.Zip;
+                    c.City = _ZipcodeDTO.City;
+                    c.State = _ZipcodeDTO.State;
+                    c.CreatedBy = _ZipcodeDTO.CreatedBy;
+                    c.CreatedDate = _ZipcodeDTO.CreatedDate;
+                    c.ModifiedBy = _ZipcodeDTO.ModifiedBy;
+                    c.ModifiedDate = _ZipcodeDTO.ModifiedDate;
+
+                    _context.Zipcodes.Update(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("DeleteZipcode/{_Zip}")]
+        public async Task<IActionResult> DeleteCourse(String _Zip)
+        {
+            try
+            {
+                Zipcode c = await _context.Zipcodes.Where(x => x.Zip == _Zip).FirstOrDefaultAsync();
+
+                if (c != null)
+                {
+                    _context.Zipcodes.Remove(c);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            catch (DbUpdateException Dex)
+            {
+                List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, Newtonsoft.Json.JsonConvert.SerializeObject(DBErrors));
+            }
+            catch (Exception ex)
+            {
+                _context.Database.RollbackTransaction();
+                List<OraError> errors = new List<OraError>();
+                errors.Add(new OraError(1, ex.Message.ToString()));
+                string ex_ser = Newtonsoft.Json.JsonConvert.SerializeObject(errors);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, ex_ser);
+            }
+
+            return Ok();
+        }
+
+
+
+    }
+}
+
